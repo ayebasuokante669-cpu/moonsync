fb82603ef54be2a3a694acc7915861f61e319ac4
ayebasuokante669-cpu <ayebasuokante@gmail.com>
2026-03-24 10:19:03 +0100

    changes

--- backend/app/ai/tts_service.py
+++ backend/app/ai/tts_service.py
@@ -1,15 +1,13 @@
-import pyttsx3
+from gtts import gTTS
 import tempfile
-import os 
-
-engine = pyttsx3.init()
+import os
 
 def text_to_speech(text: str) -> str:
     tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
     path = tmp_file.name
     tmp_file.close()
 
-    engine.save_to_file(text, path)
-    engine.runAndWait()
+    tts = gTTS(text=text, lang="en")
+    tts.save(path)
 
     return path
